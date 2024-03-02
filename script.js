ea2dd1b173dc9eaf476f4a991c73fb17adf90085
ntoghrul <85015713+ntoghrul@users.noreply.github.com>
2024-03-02 19:18:19 +0000

    fs script.js

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Fetch the quotes from the JSON file
+
     fetch('quotes.json')
         .then(response => response.json())
         .then(quotes => {
-            // Get the total number of quotes
+          
             const totalQuotes = quotes.length;
-
-            // Get the current date
             const today = new Date();
-
-            // Calculate the quote index based on the current day
             const quoteIndex = today.getDate() % totalQuotes;
-
-            // Get the quote object using the calculated index
             const todaysQuote = quotes[quoteIndex];
-
-            // Display the quote text
             document.getElementById('quote').innerText = todaysQuote.text;
         })
         .catch(error => console.error('Error fetching quotes:', error));
